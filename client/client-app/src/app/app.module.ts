8e662ffd56bb216a3c41b676b40a7f3b9fa2123a
Hugo <hugodeiro@gmail.com>
2019-06-21 08:51:28 -0300

    Changing the theme

--- client/client-app/src/app/app.module.ts
+++ client/client-app/src/app/app.module.ts
@@ -6,7 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './views/main/main.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatInputModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule, MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatInputModule
+} from '@angular/material';
 import { TodoListComponent } from './views/pages/todo/todo-list/todo-list.component';
 import { RouteService } from './services/route.service';
 import { TodoComponent } from './components/todo/todo.component';
