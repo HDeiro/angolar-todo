b2311a321ed73b82cff59599cbbefddbcd6baca2
Hugo Deiro <hugodeiro@gmail.com>
2019-06-27 09:47:10 -0300

    Form validation & ObjectUtils

--- client/client-app/src/app/components/todo/todo.component.ts
+++ client/client-app/src/app/components/todo/todo.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/todo.model';
 import { TodoService } from 'src/app/services/api/todo.service';
+import { FormErrorMatcher } from 'src/app/utilities/form-error-matcher.util';
+import { FormControl, Validators } from '@angular/forms';
+import { ObjectUtils } from 'src/app/utilities/object-utils.util';
 
 @Component({
   selector: 'app-todo',
@@ -12,6 +15,11 @@ export class TodoComponent implements OnInit {
   @Input('model') model: Todo;
   editableModel: Todo;
   isFullscreenModeActivated: boolean = false;
+  matcher = new FormErrorMatcher();
+  formControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
   constructor(
     private todoService: TodoService
@@ -21,7 +29,7 @@ export class TodoComponent implements OnInit {
   }
 
   toggleFullscreenMode() {
-    this.editableModel = this.model;
+    this.editableModel = <Todo> ObjectUtils.clone(this.model);
     this.isFullscreenModeActivated = !this.isFullscreenModeActivated;
     
     if(!this.isFullscreenModeActivated)
--- client/client-app/src/app/views/pages/todo/todo-new/todo-new.component.ts
+++ client/client-app/src/app/views/pages/todo/todo-new/todo-new.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/todo.model';
 import { TodoService } from 'src/app/services/api/todo.service';
+import { FormErrorMatcher } from 'src/app/utilities/form-error-matcher.util';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-new',
@@ -10,6 +12,11 @@ import { TodoService } from 'src/app/services/api/todo.service';
 export class TodoNewComponent implements OnInit {
 
   todoModel: Todo;
+  matcher = new FormErrorMatcher();
+  formControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
   constructor(
     private todoService: TodoService
